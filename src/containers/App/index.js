866ba54799d824ff833b77821f31b87be909a389
rram <randy.ram@caribbean-airlines.com>
2018-10-08 16:56:20 -0400

    Initial Commit

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -4,17 +4,30 @@ import theme                  from 'configs/config-theme'
 import AppBar                 from 'components/AppBar'
 import Home                   from 'containers/Home'
 import { appConfig }          from 'configs/config-main'
+import Button                 from 'components/Button'
 
 // global styles for entire app
 import './styles.scss'
 
 class App extends Component {
+  signIn = () => {
+    console.log('Click')
+    alert('Clicked')
+  }
+
   render() {
     return (
       <MuiThemeProvider theme={theme}>
         <div>
           <AppBar>{appConfig.name}</AppBar>
           <Home />
+          <Button
+            variant="fab"
+            color="primary"
+            onClick={this.signIn}
+          >
+            Sign into Apple Music
+          </Button>
         </div>
       </MuiThemeProvider>
     )
